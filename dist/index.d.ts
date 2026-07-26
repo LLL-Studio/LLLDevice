448a88479d02171bb840da6ba365234e07761cdc
cookedas1 <mulmando1991@gmail.com>
2026-07-26 10:08:42 +0900

    Add files via upload

---
+++ dist/index.d.ts
@@ -0,0 +1,11 @@
+export declare const lllDevice: {
+    readonly isMobile: any;
+    readonly isTablet: any;
+    readonly isDesktop: any;
+    readonly isTouchDevice: any;
+    readonly browser: any;
+    readonly os: any;
+    readonly isDarkMode: boolean;
+    readonly orientation: "unknown" | "landscape" | "portrait";
+};
+export default lllDevice;
